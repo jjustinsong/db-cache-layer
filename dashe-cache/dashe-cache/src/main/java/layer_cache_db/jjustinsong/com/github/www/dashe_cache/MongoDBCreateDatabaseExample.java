1611b401bae6760fd3f03cbcfdaf311b0fc3eb56
Justin Song <justin57391@gmail.com>
2024-11-10 18:16:49 -0800

    mongo initialization

--- dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/MongoDBCreateDatabaseExample.java
+++ dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/MongoDBCreateDatabaseExample.java
@@ -1,36 +1,19 @@
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
 
-public class MongoDBCreateDatabaseExample {
-    public static void main(String[] args) {
-        // Create a MongoClient instance
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+public class DatabaseInitializer implements CommandLineRunner {
+    private final MongoTemplate mongoTemplate;
+    public DatabaseInitializer(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
 
-        try {
-            // Access the database (will be created upon data insertion)
-            MongoDatabase database = mongoClient.getDatabase("myDatabase");
-
-            // Access the collection (will be created upon data insertion)
-            MongoCollection<Document> collection = database.getCollection("myCollection");
-
-            // Create a document
-            Document document = new Document("name", "Alice")
-                    .append("age", 30)
-                    .append("email", "alice@example.com");
-
-            // Insert the document into the collection
-            collection.insertOne(document);
-
-            System.out.println("Database and collection created, document inserted.");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Close the client
-            mongoClient.close();
+    @Override
+    public void run(String... args) throws Exception {
+        // Check if the collection exists; if not, create it
+        if (!mongoTemplate.collectionExists("cacheLayer")) {
+            mongoTemplate.createCollection("cacheLayer");
+            System.out.println("Collection 'cacheLayer' created in MongoDB.");
         }
     }
 }
