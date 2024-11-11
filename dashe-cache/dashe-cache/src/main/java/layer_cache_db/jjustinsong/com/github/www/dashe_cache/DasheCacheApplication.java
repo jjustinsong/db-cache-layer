b872ffacca62f94925579b54e4a0a0a49ea34d99
Justin Song <justin57391@gmail.com>
2024-11-10 17:44:34 -0800

    created spring boot

---
+++ dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/DasheCacheApplication.java
@@ -0,0 +1,13 @@
+package layer_cache_db.jjustinsong.com.github.www.dashe_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasheCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasheCacheApplication.class, args);
+	}
+
+}
---
+++ dashe-cache/dashe-cache/src/test/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/DasheCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package layer_cache_db.jjustinsong.com.github.www.dashe_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasheCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
