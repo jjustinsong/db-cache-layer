86f832d6ce324fe31ab3d7bfdbda4156329eb6dd
amach8 <98847891+amach8@users.noreply.github.com>
2024-11-11 15:06:42 -0800

    implemented key serialization:

--- dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/RedisConfig.java
+++ dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/RedisConfig.java
@@ -1,36 +1,31 @@
-package layer_cache_db.jjustinsong.com.github.www.dashe_cache;
+package com.example.rediscachedemo.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-// Uncomment the following imports if using JSON serialization
-// import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-// import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.*;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory/*
-                                                                                                * , ObjectMapper
-                                                                                                * objectMapper
-                                                                                                */) {
-
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory,
+            ObjectMapper objectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        // Uncomment the following lines to use JSON serialization
-        /*
-         * template.setKeySerializer(new StringRedisSerializer());
-         * template.setValueSerializer(new
-         * GenericJackson2JsonRedisSerializer(objectMapper));
-         * 
-         * template.setHashKeySerializer(new StringRedisSerializer());
-         * template.setHashValueSerializer(new
-         * GenericJackson2JsonRedisSerializer(objectMapper));
-         */
+        // Configure the key serializer
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+
+        // Configure the value serializer
+        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
 
         template.afterPropertiesSet();
         return template;
