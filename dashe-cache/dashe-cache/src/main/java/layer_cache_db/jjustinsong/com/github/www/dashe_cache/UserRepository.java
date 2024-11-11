db575b39d7242450717f3851487698c46be5fcf8
Justin Song <justin57391@gmail.com>
2024-11-10 19:11:26 -0800

    .

--- dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/UserRepository.java
+++ dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/UserRepository.java
@@ -1,6 +1,6 @@
 package layer_cache_db.jjustinsong.com.github.www.dashe_cache;
 
-import com.example.demo.model.User;
+import layer_cache_db.jjustinsong.com.github.www.dashe_cache.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
