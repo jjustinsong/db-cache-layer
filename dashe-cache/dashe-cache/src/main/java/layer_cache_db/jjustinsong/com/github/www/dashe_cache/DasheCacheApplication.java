3d0c6fab2e3477394a8a713159ffba270fe8be64
amach8 <98847891+amach8@users.noreply.github.com>
2024-11-11 17:25:18 -0800

    .

--- dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/DasheCacheApplication.java
+++ dashe-cache/dashe-cache/src/main/java/layer_cache_db/jjustinsong/com/github/www/dashe_cache/DasheCacheApplication.java
@@ -8,7 +8,6 @@ import redis.clients.jedis.Jedis;
 @SpringBootApplication
 @EnableCaching
 public class DasheCacheApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DasheCacheApplication.class, args);
 	}
